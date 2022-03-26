88b2d26a457cb44e59df4abcaf67cfd43044e510
Sahil Verma <sahil@softobiz.com>
2022-03-27 00:29:32 +0530

    Basket api name fix and done with dockerization

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -22,7 +22,7 @@ namespace Basket.API
         {
             services.AddStackExchangeRedisCache(options =>
             {
-                options.Configuration = Configuration.GetValue<string>("CacheSettings:ConnectionSetting");
+                options.Configuration = Configuration.GetValue<string>("CacheSettings:ConnectionString");
             });
             services.AddScoped<IBasketRepository, BasketRepository>();
 
